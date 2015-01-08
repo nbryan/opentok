7223c341be1c837684dd961be09fec67f4a48388
Nathan Bryan <nathan.bryan@gmail.com>
2015-01-08 15:00:52 -0500

    Revert "Comment out canned sessions and token code"
    This reverts commit 49ff0ab1e420196d5d91a4292b8d66705fb6274c.

--- routes/sessions.js
+++ routes/sessions.js
@@ -1,40 +1,40 @@
-var OpenTok = require('opentok');
+// var OpenTok = require('opentok');
 // var Session = require('../models/session');
-var opentok = new OpenTok('45110572', '172d9d69d70423fd6caa014d932456f9c35de748');
-// var cannedData = require('../canned_data');
+// var opentok = new OpenTok('45110572', '172d9d69d70423fd6caa014d932456f9c35de748');
+var cannedData = require('../canned_data');
 
 exports.create = function(req, res, next) {
-  opentok.createSession({mediaMode: 'routed'}, function(err, session) {
-    if (err) {
-      return next(err);
-    }
-
-    var token = opentok.generateToken(session.sessionId, {
-      role: 'publisher',
-      expireTime: (Date.now() / 1000.0) + (30 * 24 * 60 * 60)
-    });
-
-    res.status(201).send({
-      sessionId: session.sessionId,
-      token: token
-    });
-  });
-  // var sessionId = cannedData.nextSession();
-  // var token = cannedData.nextToken(sessionId);
+  // opentok.createSession({mediaMode: 'routed'}, function(err, session) {
+  //   if (err) {
+  //     return next(err);
+  //   }
   //
-  // res.status(201).send({
-  //   sessionId: sessionId,
-  //   token: token
-  // })
+  //   var token = opentok.generateToken(session.sessionId, {
+  //     role: 'publisher',
+  //     expireTime: (Date.now() / 1000.0) + (30 * 24 * 60 * 60)
+  //   });
+  //
+  //   res.status(201).send({
+  //     sessionId: session.sessionId,
+  //     token: token
+  //   });
+  // });
+  var sessionId = cannedData.nextSession();
+  var token = cannedData.nextToken(sessionId);
+
+  res.status(201).send({
+    sessionId: sessionId,
+    token: token
+  })
 };
 
 exports.show = function(req, res, next) {
-  var token = opentok.generateToken(req.params.sessionId, {
-    role: 'publisher',
-    expireTime: (Date.now() / 1000.0) + (30 * 24 * 60 * 60)
-  });
-  // var token = cannedData.nextToken(req.params.sessionId);
-  //
+  // var token = opentok.generateToken(req.params.sessionId, {
+  //   role: 'publisher',
+  //   expireTime: (Date.now() / 1000.0) + (30 * 24 * 60 * 60)
+  // });
+  var token = cannedData.nextToken(req.params.sessionId);
+
   res.render('sessions/show', {
     sessionId: req.params.sessionId,
     token: token
@@ -42,19 +42,19 @@ exports.show = function(req, res, next) {
 };
 
 exports.createToken = function(req, res, next) {
-  var token = opentok.generateToken(req.params.sessionId, {
-    role: 'publisher',
-    expireTime: (Date.now() / 1000.0) + (30 * 24 * 60 * 60)
-  });
-
-  res.status(201).send({
-    sessionId: req.params.sessionId,
-    token: token
-  });
-  // var token = cannedData.nextToken(req.params.sessionId);
+  // var token = opentok.generateToken(req.params.sessionId, {
+  //   role: 'publisher',
+  //   expireTime: (Date.now() / 1000.0) + (30 * 24 * 60 * 60)
+  // });
   //
   // res.status(201).send({
   //   sessionId: req.params.sessionId,
   //   token: token
   // });
+  var token = cannedData.nextToken(req.params.sessionId);
+
+  res.status(201).send({
+    sessionId: req.params.sessionId,
+    token: token
+  });
 };
